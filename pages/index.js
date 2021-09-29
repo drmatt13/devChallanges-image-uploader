f398dc89e4db793bcf4e5ea0f5790ae12557d312
Matthew Sweeney <Matthew.Sweeney001@gmail.com>
2021-09-29 00:04:23 -0700

    1.0

--- pages/index.js
+++ pages/index.js
@@ -2,30 +2,22 @@ import Head from 'next/head'
 
 export default function Home() {
   return <>
-    <div className="max-w-full h-24 mb-5 flex justify-between bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
-      <div className="max-h-full ml-10 flex justify-center items-center text-8xl">
-        <i className="devicon-nextjs-plain-wordmark colored cursor-pointer" />
-      </div>
-      <div className="max-h-full mr-10 flex justify-center items-center text-2xl">
-        <div className="bg-white rounded-full shadow-2xl cursor-pointer ring-1 ring-gray-100 hover:bg-green-400 hover:ring-green-400 transition duration-100 ease-in-out">
-          <i className="fas fa-shopping-cart p-3" />
+    <div className="min-h-screen w-full flex justify-center items-center bg-gray-100">
+      <div className="bg-white rounded-xl shadow-lg border flex flex-col items-center">
+        <h1 className="mt-10 text-xl text-black">
+          Upload your image
+        </h1>
+        <h2 className="mt-3 text-xs">
+          File should be .Jpeg, .Png...
+        </h2>
+        <div className="flex flex-col items-center justify-evenly mt-6 mx-10 w-64 h-40 bg-gray-100 rounded-xl border border-1 border-blue-500">
+          <i className="far fa-images text-blue-300 text-7xl" />
+          <div className="text-gray-400 text-xs select-none">Drag & Drop your image here</div>
         </div>
-      </div>
-    </div>
-    <div className="container bg-pink-100">
-      <div className="text-7xl max-w-min flex items-center">
-        <div className="animate-pulse">
-          <i className="devicon-nodejs-plain colored"/>
-        </div>
-        <div className="animate-spin-slow ml-5">
-          <i className="devicon-react-original colored"/>
-        </div>
-        <div className="animate-ping ml-9 text-5xl">
-          <i className="devicon-redux-original colored"/>
-        </div>
-        <div className="animate-bounce ml-9">
-          <i className="devicon-tailwindcss-plain colored"/>
-        </div>
+        <div className="mt-4 text-gray-400 text-xs">Or</div>
+        <button className="flex mt-4 mb-8 py-2 px-6 rounded-xl bg-blue-600 text-white hover:bg-blue-500 ">
+          Choose a file
+        </button>
       </div>
     </div>
   </>
